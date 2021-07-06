ac3e79a0b66d89a99b85d48a5448ca26ef73313c
Nikita Haltsev <n.haltsev@do-it.co>
2021-07-06 12:34:17 +0300

    comment remove

--- src/helpers/getCurrentDate.tsx
+++ src/helpers/getCurrentDate.tsx
@@ -1,5 +1,1 @@
-// export const getCurrentDate = () => {
-//   return new Date().toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: '2-digit', hour: '2-digit', minute: '2-digit' });
-// };
-
 export const getCurrentDate = () => new Date();
